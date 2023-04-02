97f3674d4e90525941ed860be6d78e4dee46209b
Rahul <rahulranjan2573@gmail.com>
2023-04-03 01:14:45 +0530

    vercel configuration

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ connectDatabase();
 import {product_route} from './routes/ProductRoute.js';
 app.use('/api/product', product_route);
 
+app.get("/", (req, res)=>{
+  res.status(200).send({ success: true, msg: "Welcome to Laxnar Enterprises Lucknow Backend"});
+})
+
 //create category route
 import product_category_routes from './routes/ProductCategoryRoute.js';
 app.use('/api/category', product_category_routes);
