02927ca004096baba58e61ca919275f0d5f63b70
Rahul <rahulranjan2573@gmail.com>
2023-05-01 02:23:51 +0530

    Added Enquiry EMail Sender Nodemailer

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import {app} from './app.js';
 import cors from 'cors'
-// const dotenv = require("dotenv");
 import dotenv from 'dotenv';
 import {connectDatabase} from './config/database.js';
-//config
+import product_category_routes from './routes/ProductCategoryRoute.js';
+import {product_route} from './routes/ProductRoute.js';
+import {enquiryRoute} from './routes/enquiryRoute.js'
 
+//config
 dotenv.config({ path: "config/config.env" });
 
 app.use(cors({
@@ -16,16 +18,18 @@ connectDatabase();
 
 
 //create product route
-import {product_route} from './routes/ProductRoute.js';
 app.use('/api/product', product_route);
+
 
 app.get("/", (req, res)=>{
   res.status(200).send({ success: true, msg: "Welcome to Laxnar Enterprises Lucknow Backend"});
 })
 
 //create category route
-import product_category_routes from './routes/ProductCategoryRoute.js';
 app.use('/api/category', product_category_routes);
+
+//Enquiry Route
+app.use('/api/support/enquiry', enquiryRoute);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is working on fine");
